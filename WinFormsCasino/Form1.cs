aa0dbf671a7fe6733c7a61a46bbada0e196ce827
Maria <xairaehot@gmail.com>
2025-04-23 12:24:16 +0200

    Added WinFormsCasino

---
+++ WinFormsCasino/Form1.cs
@@ -0,0 +1,12 @@
+using ClassLibraryCasino;
+
+namespace WinFormsCasino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
